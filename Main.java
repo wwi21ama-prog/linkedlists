285ecce35ea6807dac362602ebde21f44310be4d
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-14 23:24:42 +0100

    Noch mehr Werte in der main() an die Liste anhängen und ausgeben.

--- Main.java
+++ Main.java
@@ -16,5 +16,18 @@ public class Main {
         // Das Element und seine Gültigkeit wieder ausgeben.
         System.out.println(e);
         System.out.println(e.isValid());
+
+        // Eine Reihe weiterer Werte am Ende hinzufügen.
+        e.next.setData(25, 17);
+        e.next.next.setData(75, 107);
+        e.next.next.next.setData(38, 55);
+
+        // Die ganze Liste noch einmal ausgeben.
+        System.out.println(e); // (42,15)
+        System.out.println(e.next); // (25,17)
+        System.out.println(e.next.next); // (75,107)
+        System.out.println(e.next.next.next); // (38, 55)
+        System.out.println(e.next.next.next.next); // (0,0) (ungültiges Element am Ende)
+        System.out.println(e.next.next.next.next.next); // null
     }
 }
