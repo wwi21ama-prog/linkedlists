00a1b769fcc3b06916e5372f492a53754a8f9e82
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-14 23:11:26 +0100

    Doku hinzugefügt.

--- Element.java
+++ Element.java
@@ -1,4 +1,17 @@
+/**
+ * Datentyp für Elemente einer verketteten Liste.
+ */
 public class Element {
+    /*
+     * Attribute:
+     *   - Schlüssel: Hiernach wird später sortiert werden.
+     *   - Wert: Die eigentlichen Daten der Elemente.
+     * 
+     *     (Diese beiden Einträge sind nur für den Anfang int.
+     *      Später werden wir sie durch sinnvollere Daten ersetzen.)
+     *   
+     *   - next: Der Nachfolger des aktuellen Elements in der Liste.
+     */
     int key;
     int value;
     
--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        
+        // Ein Element erzeugen.
         Element e = new Element();
 
+        // Das Element ausgeben.
         System.out.println(e);
     }
 }
