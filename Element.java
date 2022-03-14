c883af0f42292bfa379ec588c40aa49b4bb4cfff
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-14 23:13:32 +0100

    String-Ausgabe-Methode hinzugefügt.

--- Element.java
+++ Element.java
@@ -16,4 +16,13 @@ public class Element {
     int value;
     
     Element next;
+
+    /**
+     * Methode für die String-Ausgabe.
+     * Durch die Existenz dieser Funktion gibt System.out.println()
+     * nicht nur eine Adresse aus, sondern wirklich das Schlüssel-Wert-Paar.
+     */
+    public String toString() {
+        return "(" + key + "," + value + ")";
+    }
 }
